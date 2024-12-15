4ba0af22e4d2c201902bb55865fc2eb9a13f87e9
Eren Copcu <eren.copcu@student.uantwerpen.be>
2024-12-15 11:33:12 +0100

    ignore warnings

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import sys
 import os
 import logging
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 logging.basicConfig(level=logging.DEBUG, filename='run.log', filemode='w')
 
